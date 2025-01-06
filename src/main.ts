b08b3b6fc3d72a91fb612b2935f8a5929cdbbf88
spectr-e <devs.josia@gmail.com>
2025-01-06 13:03:05 +0300

    feat: Add submit event listener to the form that creates an object of input and stores it to an array

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,22 @@ const form = document.querySelector<HTMLFormElement>("#new-todo-form")
 const input = document.querySelector<HTMLInputElement>("#todo-input")!
 const list = document.querySelector<HTMLUListElement>("#list")!
 let todos: Todo[] = []
+
+form?.addEventListener("submit", (e) => {
+	e.preventDefault()
+	const inputVal = input.value.trim()
+
+	if (inputVal === "") {
+		return
+	}
+
+	const todo: Todo = {
+		id: crypto.randomUUID(),
+		text: inputVal,
+		completed: false,
+	}
+
+	todos.push(todo)
+	// renderTodo(todo)
+	input.value = ""
+})
